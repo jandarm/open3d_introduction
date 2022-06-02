372d5c8a69cf9a5741683ee30df2ab96300a09a3
Daniel <50790074+jandarm@users.noreply.github.com>
2022-06-02 13:58:25 +0300

    Add files via upload

--- construct_mesh.py
+++ construct_mesh.py
@@ -41,9 +41,37 @@ density_mesh.vertices = mesh1.vertices
 density_mesh.triangles = mesh1.triangles
 density_mesh.triangle_normals = mesh1.triangle_normals
 density_mesh.vertex_colors = o3d.utility.Vector3dVector(density_colors)
-o3d.visualization.draw_geometries([density_mesh])
+# o3d.visualization.draw_geometries([density_mesh])
 
 vertices_to_remove = densities < np.quantile(densities, 0.01)
 mesh1.remove_vertices_by_mask(vertices_to_remove)
 
-o3d.visualization.draw_geometries([mesh1])
+test1 = o3d.io.read_triangle_mesh("Goat skull.ply")
+test2 = o3d.io.read_triangle_mesh("Goat skull.ply")
+test3 = o3d.io.read_triangle_mesh("Goat skull.ply")
+test2 = test2.translate((0.3, 0, 0), relative=False)
+test3 = test3.translate((0, 0.3, 0), relative=False)
+
+# r = test2.get_rotation_matrix_from_xyz((np.pi / 2, 0, np.pi / 4))
+# test2.rotate(r, center=(0, 0, 0))
+test2.rotate(test2.get_rotation_matrix_from_xyz((0, 0, 3)), center=test2.get_center())
+test1.rotate(test1.get_rotation_matrix_from_xyz((1, 0, 0)), center=test1.get_center())
+test1.scale(0.9, center=test1.get_center())
+
+
+# axis_pcd = o3d.geometry.TriangleMesh.create_coordinate_frame()
+# axis_pcd.scale(55.0, center=test1.get_center())
+# test1_center = center=test1.get_center()
+# axis_pcd.translate(test1_center)
+# Визуализатор 
+# vis = o3d.visualization.VisualizerWithEditing()
+# vis.create_window(window_name="Open3D1")
+# vis.get_render_option().point_size = 3
+
+# vis.add_geometry(axis_pcd)
+# vis.add_geometry(test1)
+# vis.add_geometry(test2)
+# vis.add_geometry(test3)
+# vis.run()
+
+o3d.visualization.draw_geometries([test1, test2, test3])
